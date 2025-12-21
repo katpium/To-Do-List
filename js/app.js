31b5c1014c8e687c4c81a939cee93c6f45dc8bf2
katpium <uwuing88@gmail.com>
2025-12-20 22:58:22 -0800

    starting

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById(".list");
+const input = document.getElementById("input");
+
+const  CHECK = "fa-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+ 
+//showing Dates
+const options = {weekday: "long", month: "short", day: "numeric"};
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
