70b728c0a1a9937bd59284f08409f1fdc35db2a9
katpium <uwuing88@gmail.com>
2025-12-20 23:09:58 -0800

    list add

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // CODE EXPLAINED channel
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
-const list = document.getElementById(".list");
+const list = document.getElementById("list");
 const input = document.getElementById("input");
 
 const  CHECK = "fa-circle";
@@ -12,3 +12,16 @@ const LINE_THROUGH = "lineThrough";
 const options = {weekday: "long", month: "short", day: "numeric"};
 const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+function addTodo(toDo){
+
+    const item =`<li class ="item">
+                    <i class ="fa fa-circle-thin co" job ="complete" id ="0"></i>
+                    <p class ="text">${toDo}</p>
+                    <i class ="fa fa-trash-o de" job ="delete" id ="0"></i>
+                    </li>`;
+    const position = "beforeend";
+    list.insertAdjacentHTML(position,item);
+}
+
+addTodo("drink coffee");
