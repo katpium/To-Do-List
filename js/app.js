e22bf6f7288f11d7be050f0a2e51337cca3517b7
katpium <uwuing88@gmail.com>
2026-01-08 16:47:38 -0800

    add

--- js/app.js
+++ js/app.js
@@ -4,24 +4,105 @@ const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
-const  CHECK = "fa-circle";
+const  CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
  
+let LIST;
+let id;
+let data = localStorage.getItem("TODO");
+
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+}else{
+    LIST = [];
+    id = 0;
+}
+
+function loadList(array){
+    array.forEach(function(item){
+        addTodo(item.name, item.id, item.done, item.trash)
+    });
+}
+
+//Clear button
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+});
+
 //showing Dates
 const options = {weekday: "long", month: "short", day: "numeric"};
 const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
-function addTodo(toDo){
+function addTodo(toDo, id, done, trash){
+
+    if(trash){
+        return;
+    }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done? LINE_THROUGH : "";
 
     const item =`<li class ="item">
-                    <i class ="fa fa-circle-thin co" job ="complete" id ="0"></i>
-                    <p class ="text">${toDo}</p>
-                    <i class ="fa fa-trash-o de" job ="delete" id ="0"></i>
+                    <i class ="fa ${DONE} co" job ="complete" id ="${id}"></i>
+                    <p class ="text ${LINE}">${toDo}</p>
+                    <i class ="fa fa-trash-o de" job ="delete" id ="${id}"></i>
                     </li>`;
     const position = "beforeend";
     list.insertAdjacentHTML(position,item);
 }
 
-addTodo("drink coffee");
+document.addEventListener("keyup", function(event){
+    if(event.keyCode === 13){
+        const toDo = input.value;
+        if(toDo){
+            addTodo(toDo, id, false, false);
+
+            LIST.push({
+                name : toDo,
+                id : id,
+                done : false,
+                trash : false
+            });
+            //add to local storage 
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+        }
+        input.value = "";
+    }
+})
+
+//COMPLETE FUNCTION
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+    LIST[element.id].done = LIST[element.id].done? false : true;
+}
+
+function removeTodo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+
+    LIST[element.id].trash = true;
+}
+
+document.addEventListener("click", function (event){
+    const element = event.target;
+    const elementJob = element.getAttribute("job");
+
+    if(!elementJob){
+        return;
+    }
+
+    if(elementJob === "complete"){
+        completeToDo(element);
+    } else if (elementJob === "delete"){
+        removeTodo(element);
+    }
+    //add to local storage 
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
